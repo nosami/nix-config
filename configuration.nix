3df9d3fe02dc499d8392f1c400e1fbfc68dd8c78
nosami <jasonimison@gmail.com>
2015-12-27 14:39:46 +0000

    add more packages

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,7 @@
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda";
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Select internationalisation properties.
@@ -33,37 +33,60 @@
   # $ nix-env -qaP | grep wget
 
   environment.systemPackages = with pkgs; [
-    wget i3status dmenu
-    gcc clang
-    cmake gnumake
-    wget unzip p7zip
-    vim neovim emacs
-    git subversion
-    xorg.xrandr
-    chromium firefoxWrapper
+    bashCompletion
+    chromium 
+    clang
+    cmake 
+    compton
+    dmenu
+    emacs
+    firefoxWrapper
+    gcc 
+    git 
+    gnumake
+    i3
+    i3status
+    neovim 
+    nitrogen
+    open-vm-tools
+    p7zip
+    qutebrowser
+    silver-searcher
+    subversion
     termite
+    unzip 
+    vim 
+    wget 
+    xorg.xrandr
   ];
 
   # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services = {
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+      # Enable the OpenSSH daemon.
+      openssh.enable = true;
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-  services.xserver.windowManager.i3.enable = true;
+      # Enable CUPS to print documents.
+      # services.printing.enable = true;
 
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.kdm.enable = true;
-  services.xserver.windowManager.default = "i3";
-  services.xserver.desktopManager.default = "none";
-  services.xserver.desktopManager.xterm.enable = false;
+    xserver = {
+      # Enable the X11 windowing system.
+      enable = true;
+      # services.xserver.layout = "us";
+      # services.xserver.xkbOptions = "eurosign:e";
+      windowManager.i3.enable = true;
 
+      # Enable the KDE Desktop Environment.
+      # services.xserver.displayManager.kdm.enable = true;
+      windowManager.default = "i3";
+      desktopManager.default = "none";
+      desktopManager.xterm.enable = false;
+      synaptics = {
+        enable = true;
+      };
+    };
+  };
   # Define a user account. Don't forget to set a password with ‘passwd’.
    users.extraUsers.jason = {
      isNormalUser = true;
@@ -73,6 +96,6 @@
 
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "15.09";
-  services.vmwareGuest.enable = true;
+  fonts.fontconfig.dpi = 200;
 
 }
